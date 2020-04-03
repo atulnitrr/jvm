3765286dbadb00ce3f6c59bd118522a00221f96f
Atul Kumar <atulkumar458@gmail.com>
2020-04-03 12:57:51 +0530

    Added dummy

--- src/main/java/com/atul/jvm/jvm/JvmApplication.java
+++ src/main/java/com/atul/jvm/jvm/JvmApplication.java
@@ -8,6 +8,16 @@ public class JvmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JvmApplication.class, args);
+		System.out.println("app is up and running --> ");
+		int data  = 7;
+		data  = value(data);
+		System.out.println(data);
+	}
+
+	private static int value(int data) {
+		int oldValue = data + 3;
+		int neValue = oldValue * 2;
+		return neValue;
 	}
 
 }
--- src/test/java/com/atul/jvm/jvm/DummyTest.java
+++ src/test/java/com/atul/jvm/jvm/DummyTest.java
@@ -1,4 +1,11 @@
 package com.atul.jvm.jvm;
 
+import org.junit.jupiter.api.Test;
+
 public class DummyTest {
+
+    @Test
+    void test_Data() {
+         String ss = "fggg";
+    }
 }
