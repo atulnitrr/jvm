b4970ed2424f5c4bd61ca0da88125e6b060c1512
Atul Kumar <atulkumar458@gmail.com>
2020-04-03 11:25:44 +0530

    Initial commit

---
+++ src/test/java/com/atul/jvm/jvm/JvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.jvm.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
